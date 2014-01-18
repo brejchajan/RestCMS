bc71b7f981ffeb896e43dc833b7cd05c30102d07
Jan Brejcha <brejchaja@gmail.com>
2014-01-18 21:42:33 +0100

    Helper fixed single result in getNextId method
    added method for getting template id.

--- src/cz/brejchajan/restcms/Helper.php
+++ src/cz/brejchajan/restcms/Helper.php
@@ -11,10 +11,33 @@ class Helper{
 		       	->orderBy('t.id', 'DESC')
 			->setMaxResults('1');
 		$q = $qb->getQuery();
-		$lastTemplate = $q->getSingleResult();
+		$res = $q->getResult();
+		$lastTemplate = $res[0];
+		if (!$lastTemplate || $lastTemplate == NULL){
+			return 0;
+		}
 		$id = $lastTemplate->getId();
 		return $id + 1;
 	}
+
+	public static function getTemplateId($vendor, $name, $em){
+		$qb = $em->createQueryBuilder();
+		$qb->select('t')
+			->from('Template', 't')
+			->where($qb->expr()->andX(
+				$qb->expr()->eq("t.vendor", "'" . $vendor  . "'"),
+				$qb->expr()->eq("t.name", "'" . $name  . "'")
+			));
+		$q = $qb->getQuery();
+		$res = $q->getResult();
+
+		if (!$res || $res == NULL){
+			return -1;
+		}
+		return $res[0]->getId();
+	}
+
+
 }
 ?>
 			
